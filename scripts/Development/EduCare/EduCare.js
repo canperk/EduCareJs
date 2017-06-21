da152ea0c154218ce47011d1a2c2648dd8be3126
Can PERK <canperk@outlook.com>
2017-06-21 17:17:51 +0300

    Guid generation and string is null or empty support

--- scripts/Development/EduCare/EduCare.js
+++ scripts/Development/EduCare/EduCare.js
@@ -12,4 +12,17 @@ var EC = EC || {};
 
         return Object.create(type.prototype, obj);
     }
+
+    EC.NewGuid = function () {
+        var _func = function () {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        };
+        var guid = (_func() + _func() + "-" + _func() + "-4" + _func().substr(0, 3) + "-" + _func() + "-" + _func() + _func() + _func()).toLowerCase();
+        return guid;
+    }
+
+    EC.IsNullOrEmpty = function (text) {
+        var r = !text || !/[^\s]+/.test(text);
+        return r;
+    }
 })();
